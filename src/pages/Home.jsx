2e3d056d90dd88466dfba24a52ee22ee88242e82
Alcedepradera <pablo.deleon@estudiantes.utec.edu.uy>
2025-05-14 11:27:31 -0300

    Update Home.jsx
    Componenetes de ejemplo comentados. A ver si no sale el Patrcio Rey

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,10 +22,10 @@ const Home = () => {
       <div className="section-container">
         {/* Hero Section */}
          <div>
-      <Hero />
-    </div>
+          <Hero/>
+        </div>
 
-     
+     {/* 
         <div className="text-center mb-16">
 
 
@@ -50,9 +50,16 @@ const Home = () => {
             />
           </div>
         </div>
+     */}
+
 
         {/* Features Grid */}
+
+
+        
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+          {/*}
           <Card className="card-hover">
             <div className="flex flex-col items-center text-center">
               <i className="pi pi-shield text-4xl text-primary-500 mb-4"></i>
@@ -62,11 +69,14 @@ const Home = () => {
               </p>
             </div>
           </Card>
+          
+
+          
 
           <Card className="card-hover">
             <div className="flex flex-col items-center text-center">
               <i className="pi pi-bolt text-4xl text-primary-500 mb-4"></i>
-              <h3 className="heading-secondary">Alto Rendimiento</h3>
+              <h3 className="heading-secondary">En construccion</h3>
               <p className="text-body">
                 Optimizado para ofrecer la mejor velocidad y eficiencia.
               </p>
@@ -82,10 +92,16 @@ const Home = () => {
               </p>
             </div>
           </Card>
+          */}
+
+
+
         </div>
 
         {/* Call to Action Section */}
         <div className="mt-16 text-center">
+
+          {/*
           <Card className="bg-primary-600 text-white max-w-4xl mx-auto">
             <div className="py-8">
               <h2 className="text-3xl font-bold mb-4">¿Listo para comenzar?</h2>
@@ -105,8 +121,12 @@ const Home = () => {
                   onClick={() => navigate('/VideoCurso')}
                 />
               </div>
+
+
             </div>
           </Card>
+          */}
+
         </div>
       </div>
     </div>
